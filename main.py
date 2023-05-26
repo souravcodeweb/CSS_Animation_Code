5cfe18cfb9ba7f5b146268fb5d86a2a606854bf4
sourav <souravmandal909p@gmail.com>
2023-05-26 12:43:41 +0530

    add the python animation acode

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ t.speed(0)
 n = 70
 h = 0
 for i in range (360):
-    c = colorsys.hsv_to_rgb(h, 0.8)
+    c = colorsys.hsv_to_rgb(h, 1, 0.8)
     h+= 1/n
     t.color(c)
     t.left(1)
     t.fd(1)
     for j in range (2):
         t.left(2)
-        t.circle(10)
+        t.circle(100)
